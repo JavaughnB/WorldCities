dcbab48684d6d192e6a9c1b62cfff4e9a6c30f5d
JavaughnB <javaughnboothe14@gmail.com>
2024-06-01 15:29:18 -0400

    WorldCitiesRepo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClient,HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { AngularMaterialModule } from './angularmaterial.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CitiesComponent } from './cities/cities.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavMenuComponent,
+    CitiesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/cities/cities.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { City } from './city';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+@Component({
+  selector: 'app-cities',
+  templateUrl: './cities.component.html',
+  styleUrls: ['./cities.component.scss']
+})
+export class CitiesComponent implements OnInit {
+  public displayedColumns :string[] =['id','name','lat','lon'];
+  public cities! : MatTableDataSource<City> ;
+  @ViewChild(MatPaginator) paginator!:MatPaginator
+  url : string =environment.apiBaseUrl + 'cities'
+
+  constructor(private http: HttpClient) { 
+
+  }
+
+  ngOnInit() {
+    this.http.get<City[]>(this.url).subscribe(result =>{
+      this.cities= new MatTableDataSource<City>(result);
+      this.cities.paginator = this.paginator;
+    },error => console.error(error))
+    
+ 
+  }
+}
+
